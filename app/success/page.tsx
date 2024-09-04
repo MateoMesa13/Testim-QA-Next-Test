1a13e81035b3b7f901a79f92c914cd4d091c3da3
MateoMesa13 <mateo@aktiecar.com>
2024-09-04 10:46:19 -0500

    Some changes were reverted and tailwindcss was fixed

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,7 +1,28 @@
-export default function Success() {
+"use client";
+
+import addNotification from "react-push-notification";
+
+const Success = () => {
+  const buttonClick = () => {
+    addNotification({
+      title: "¡Bienvenido!",
+      message: "Has iniciado sesión correctamente",
+      theme: "darkblue",
+      native: true,
+    });
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-2xl font-bold">Has iniciado sesión correctamente</h1>
+      <button
+        onClick={buttonClick}
+        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 transition-colors duration-300"
+      >
+        Click Me
+      </button>
     </main>
   );
-}
+};
+
+export default Success;
